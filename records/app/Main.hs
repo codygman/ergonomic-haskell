8da0576a4ee5b62ffb066982be3bb96c4db374c8
codygman <cody@codygman.dev>
2020-09-08 10:50:58 -0500

    updates

--- records/app/Main.hs
+++ records/app/Main.hs
@@ -12,7 +12,7 @@ data Dog = Dog {name :: String, age :: Int}
 
 makeFieldLabelsWith noPrefixFieldLabels ''Dog
 
-data CatBall = CatBall { name :: String, ballType :: String }
+data CatBall = CatBall {name :: String, ballType :: String}
 
 makeFieldLabelsWith noPrefixFieldLabels ''CatBall
 
@@ -25,18 +25,18 @@ main =
   let person = Person "codygman" 1000
       dog = Dog "doggo" 5
       cat = Cat "doggo" 5 (CatBall "doggos" "tennis ball")
-   in if person ^. #name == "codygman"
-        then do
-          pure ()
-          putStrLn $ "Good day, " ++ person ^. #name
-          putStrLn $ "Your dog's name: " ++ dog ^. #name
-          putStrLn $ "Your cat's name: " ++ cat ^. #name
-          if cat ^. #favoriteBall % #name == "doggos" then do -- works
-          -- if view (#favoriteBall % #name) cat == "doggos" then do
-            putStrLn "One second.. there's been a mix up it seems!"
-            let fixedCat = cat & #favoriteBall % #name .~ "cattos"
-                               & #favoriteBall % #ballType .~ "yarn ball"
-            putStrLn $ "Your cat's favorite ball type: " ++ fixedCat ^. #name
-            pure ()
-            else putStrLn $ "Your cat's name: " ++ cat ^. #name
-        else pure ()
+   in do
+        if person ^. #name == "codygman"
+          then do
+            putStrLn $ "Good day, " ++ person ^. #name
+            putStrLn $ "Your dog's name: " ++ dog ^. #name
+            putStrLn $ "Your cat's name: " ++ cat ^. #name
+            if cat ^. #favoriteBall % #name == "doggos"
+              then do
+                putStrLn "One second.. there's been a mix up it seems!"
+                let fixedCat =
+                      cat & #favoriteBall % #name .~ "cattos"
+                          & #favoriteBall % #ballType .~ "yarn ball"
+                putStrLn $ "Your cat's favorite ball type: " ++ fixedCat ^. #name
+              else putStrLn $ "Your cat's name: " ++ cat ^. #name
+          else pure ()
